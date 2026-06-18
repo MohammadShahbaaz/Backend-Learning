5751470c7e1ce1b80193dbc0662e4918859fb213
MohammadShahbaaz <shahbaazmohd784@gmail.com>
2026-06-18 13:50:27 +0530

    Decorators

--- employee.py
+++ employee.py
@@ -2,32 +2,55 @@ class Employee:
     def __init__(self, name, salary):
         if not isinstance(name, str):
             raise TypeError("Name must be a string")
+
         if salary < 0:
             raise ValueError("Salary can't be negative")
-        
+
         self.name = name
         self.__salary = salary
 
-    def get_salary(self):
-        print(f"{self.name}'s salary is: {self.__salary}")
+    @property
+    def salary(self):
+        return self.__salary
+
+    @salary.setter
+    def salary(self, salary):
+        if salary < 0:
+            raise ValueError("Salary can't be negative")
+        self.__salary = salary
 
     def give_raise(self, percentage):
         self.__salary += self.__salary * percentage / 100
 
+    @staticmethod
+    def employment_type():
+        return "Full-Time Employee"
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data["name"], data["salary"])
+
+
 class Manager(Employee):
     pass
 
-try:
-    e1 = Employee(123, 20000)
-except TypeError as e:
-    print(f"Type error: {e}")
 
-try:
-    e2 = Employee("Shahbaaz", -5000)
-except ValueError as e:
-    print(f"Value error: {e}")
+e1 = Employee("Shahbaaz", 20000)
 
-e3 = Employee("Shahbaaz", 20000)
-e3.get_salary()
-e3.give_raise(20)
-e3.get_salary()
+print(e1.salary)
+
+e1.give_raise(20)
+
+print(e1.salary)
+
+print(Employee.employment_type())
+
+employee_data = {
+    "name": "Vegeta",
+    "salary": 50000
+}
+
+e2 = Employee.from_dict(employee_data)
+
+print(e2.name)
+print(e2.salary)
